1fcd619e32c621bae94d7364172c0b9f16d6dcbd
Sean Dietrich <sean.dietrich@inresonance.com>
2017-03-31 13:15:07 -0700

    Initial Development Commit
    * Wrote TestUpstreamCommand class
    * Updated Readme.md file to show examples and other additional information

--- README.md
+++ README.md
@@ -2,10 +2,85 @@
 
 [![Terminus v1.x Compatible](https://img.shields.io/badge/terminus-v1.x-green.svg)](https://github.com/terminus-plugin-project/terminus-upstream-testing/tree/1.x)
 
+| Option                | Description    |
+| --------------------- | -------------- |
+| --repo                | Repository to use and pull code from. When omitted this will use the sites upstream |
+| --branch              | Branch from the repository to use. |
+| --env                 | Environment to use for testing |
+| --copy                | Environment to copy database and files from |
+| --teardown            | Tear down the environment before applying any changes |
+| --rebuild             | Rebuild the database and files |
+| --username            | Change user 1's username |
+| --password            | Change user 1's password |
+
+## Slack Integration
+
+Integration with [Slack](https://slack.com) using [incoming webhooks](https://my.slack.com/services/new/incoming-webhook)
+
+| Option                | Description |
+| --------------------- | ----------- |
+| --slack_url           | Slack URL to post to |
+| --slack_channel       | Slack Channel to post message to. Should be prefixed with \# |
+| --slack_message       | Message to post to slack. Following variables are available {env} {site_id} {site_name} {url}|
+| --slack_username      | Username to push through as slack message |
+| --slack_icon          | Icon to use for slack message |
+
 ## Examples
 ### Default Running
+The following will apply all updates from the master branch of the site's upstream to the upstream multi-dev 
 ```
 $ terminus site:upstream:test companysite-33
+```
+
+### Separate Upstream
+If the `--repo` argument is omitted the plugin with default to the site's upstream git repository. Otherwise a git
+url could be used to pull from a completely different repo.
+```
+$ terminus site:upstream:test companysite-33 --repo="https://github.com/pantheon-systems/drops-7.git"
+```
+
+### Specifying the environment to copy database and files
+The `--copy` argument will allow you to specify the environment to bring the database and files from. This environment
+must already be initialized. Otherwise, the live/test/dev environments will be used respectively  
+```
+$ terminus site:upstream:test companysite-33 --copy="dev"
+```
+
+### Upstream Branch
+To specify a particular branch that code would live in the `--branch` argument could be used. That branch will need to exist
+within the upstream or `--repo` repository.
+```
+$ terminus site:upstream:test companysite-33 --branch="v1.3.3"
+```
+
+### Rebuild
+To rebuild or copy the database and files from a particular environment use the `--rebuild` flag as it will dictate
+that you want to completely pull new content. In conjunction with `--copy` these could be helpful in automation if
+wanting to specify the environment that data should be rebuilt from.
+```
+$ terminus site:upstream:test companysite-33 --rebuild
+```
+
+### Reset User 1 Username and Password
+The following will reset the username and password of the user 1 account. This is helpful if you would like a standard
+account username and password for any other sort of automated testing like behat. Each argument may be used independently
+and do not need to be combined. 
+
+#### Reset Username
+```
+$ terminus site:upstream:test companysite-33 --username="admin"
+```
+
+#### Reset Password
+```
+$ terminus site:upstream:test companysite-33 --username="admin"
+```
+
+### Slack Notification
+There is an ability to have a slack message sent to a particular channel to notify when an environment has been updated.
+This is particularly useful if and when you are using this plugin for any sort of Continuous Integration work.
+```
+$ terminus site:upstream.test companysite-33 --slack-url="[REDACTED]" --slack-channel="#general"
 ```
 
 ## Installation
@@ -14,6 +89,17 @@ For help installing, see [Manage Plugins](https://pantheon.io/docs/terminus/plug
 mkdir -p ~/.terminus/plugins
 composer create-project -d ~/.terminus/plugins terminus-plugin-project/terminus-upstream-testing:~1
 ```
+
+## Install Notes
+Occasionally you may get this issue:
+```
+[Pantheon\Terminus\Exceptions\TerminusException]
+  The plugin terminus-plugin-project/terminus-upstream-testing-plugin has installed the project guzzlehttp/psr7: 1.4.2, but Terminus has installed guzzlehttp/psr7: 1.4.1. To resolve this, try running 'com
+  poser update' in both the plugin directory, and the terminus directory.
+```
+
+The way to solve this will be to go to your terminus project and run `composer update`. If you globally require it you
+will need to go directly into terminus it's self as the lock file within the terminus may contain an older version of guzzle/psr7.
 
 ## Help
 Run `terminus list site:upstream:test` for a complete list of available commands. Use `terminus help <command>` to get help on one command.
---
+++ src/Commands/TestUpstreamCommand.php
@@ -0,0 +1,253 @@
+<?php
+
+namespace Pantheon\TerminusUpstreamDev\Commands;
+
+use Pantheon\Terminus\Commands\TerminusCommand;
+use Pantheon\Terminus\Site\SiteAwareInterface;
+use Pantheon\Terminus\Site\SiteAwareTrait;
+use Pantheon\Terminus\Exceptions\TerminusException;
+use Maknz\Slack\Client;
+
+
+/**
+ * Class TestUpstreamCommand
+ */
+class TestUpstreamCommand extends TerminusCommand implements SiteAwareInterface
+{
+  use SiteAwareTrait;
+
+  /** @var \Pantheon\Terminus\Models\Site $site */
+  protected $site;
+
+  /**
+   * Pushed upstream updates from a specific branch to a multi-dev
+   *
+   * @authorize
+   *
+   * @command site:upstream:test
+   *
+   * @param string $site_id Site in the format `site-name`
+   * @option string $env Environment ID to pull tests into (optional) (Default: upstream)
+   * @option string $copy Environment ID to copy db and files from (optional)
+   *  (Defaults to highest initialized environment live/test/dev respectively)
+   * @option boolean $teardown Delete the env and branch before starting (optional)
+   * @option boolean $rebuild Clone database and files from $copy environment (optional)
+   * @option string $repo Git Repository to pull code from (optional) (Default: Site upstream repo)
+   * @option string $branch Branch from $repo to pull code from (optional) (Default: master)
+   * @option string $username Username to reset user 1 to (optional)
+   * @option string $password Password to reset user 1 to (optional)
+   * @option string $slack_url Slack URL to post to (optional)
+   * @option string $slack_channel Slack channel to post to (optional)
+   * @option string $slack_message Slack Message to post (optional) (Default: #general)
+   * @option string $slack_username Slack User to post as (optional)
+   * @option string $slack_icon Slack Icon to user for user (optional)
+   *
+   * @usage terminus site:upstream:test <site_id>
+   * @usage terminus site:upstream:test <site_id> --env="<env>"
+   * @usage terminus site:upstream:test <site_id> --slack_url="<url>" --slack_channel="<channel>"
+   * @usage terminus site:upstream:test <site_id> --repo="<url>" --branch="<branch>"
+   */
+  public function testUpdate($site_id, $options = [
+    'repo' => null,
+    'env' => 'upstream',
+    'copy' => null,
+    'teardown' => false,
+    'branch' => 'master',
+    'rebuild' => false,
+    'username' => null,
+    'password' => null,
+    'slack_message' => null,
+    'slack_url' => null,
+    'slack_channel' => null,
+    'slack_username' => 'Pantheon',
+    'slack_icon' => ':computer:'
+  ]){
+    $this->site = $this->sites->get($site_id);
+    $settings = $this->site->get('settings');
+    $data = $this->site->serialize();
+
+    $site_name = $data['label'];
+
+    $repo = $options['repo'];
+    $branch = $options['branch'];
+
+    if(is_null($repo)){
+      $upstream_data = $this->site->getUpstream();
+      $repo = $upstream_data->get('url');
+    }
+
+    $this->log()->notice('Repository being used: {repo}', ['repo' => $repo]);
+
+    $env = strtolower($options['env']);
+    $env = preg_replace("/[^A-Za-z0-9\-]/", "", $env);
+    $env = substr($env, 0, 11);
+    if(in_array($env, ['dev', 'test', 'live'])){
+      throw new TerminusException('Provided environment must not be dev, test, or live');
+    }
+
+    if ((count($this->site->getEnvironments()->multidev()) >= $settings->max_num_cdes) && (!$this->site->getEnvironments()->get($env)->isInitialized())){
+      throw new TerminusException('Max Number of Multi-Devs and {env} is not initialized', ['env' => $env]);
+    }
+
+    $this->log()->notice('Environment being used: {env}', ['env' => $env]);
+
+    $copy = $options['copy'];
+    if(is_null($copy)){
+      if($this->isInitialized('dev')){
+        $copy = 'dev';
+      }
+      if($this->isInitialized('test')){
+        $copy = 'test';
+      }
+      if($this->isInitialized('live')){
+        $copy = 'live';
+      }
+    }else{
+      if(!$this->isInitialized($copy)){
+        throw new TerminusException('{copy} is not an initialized environment with a database and files.',['copy' => $copy]);
+      }
+    }
+
+    $this->log()->notice('Environment being copied from: {env}', ['env' => $copy]);
+
+    if($this->site->getEnvironments()->has($env) && $options['teardown']){
+      $this->log()->notice('{env} is initialized and will be torn down', ['env' => $env]);
+      $this->site->getEnvironments()->get($env)->delete(['delete_branch' => true]);
+    }
+
+    if(!$this->site->getEnvironments()->has($env)){
+      $this->log()
+        ->notice('Creating multi-dev named {env} copying data from {copy}', [
+          'env' => $env,
+          'copy' => $copy
+        ]);
+
+      /** @var \Pantheon\Terminus\Models\Environment $copy_env */
+      $copy_env = $this->site->getEnvironments()->get($copy);
+      $workflow = $this->site->getEnvironments()->create($env, $copy_env);
+      while (!$workflow->checkProgress()) {}
+      $this->log()->notice($workflow->getMessage());
+    }
+
+    $site_git = $this->site->getEnvironments()->get('dev')->gitConnectionInfo()['url'];
+
+    $git_location = '/tmp/' . $site_id;
+    $this->passthru("rm -rf {$git_location}");
+    $this->log()->notice('Cloning Repository');
+    $this->passthru("git clone {$site_git} {$git_location}");
+    $this->log()->notice('Repository cloned');
+    $this->log()->notice('Checking out branch {env}', ['env' => $env]);
+    $this->passthru("git --git-dir='{$git_location}/.git' checkout -b {$env}");
+    $this->log()->notice('Merging in master environment');
+    $this->passthru("git --git-dir='{$git_location}/.git' merge master");
+    $this->log()->notice('Pulling repo {repo} and branch {branch}', ['repo' => $repo, 'branch' => $branch]);
+    $this->passthru("git --git-dir='{$git_location}/.git' pull --no-edit --commit -X theirs {$repo} {$branch}" );
+    $this->log()->notice('Pushing to origin {env}', ['env' => $env]);
+    $this->passthru("git --git-dir='{$git_location}/.git' push origin {$env}");
+    $this->log()->notice('Deleting repository from {location}', ['location' => $git_location]);
+    $this->passthru("rm -rf {$git_location}");
+
+    if($options['rebuild']){
+      $workflow = $this->site->getEnvironments()->get($env)->cloneFiles($copy);
+      $this->log()->notice(
+        "Cloning files from {from_name} environment to {target_env} environment",
+        compact(['from_name', 'target_env'])
+      );
+      while (!$workflow->checkProgress()) {}
+      $this->log()->notice($workflow->getMessage());
+
+      $workflow = $this->site->getEnvironments()->get($env)->cloneDatabase($copy);
+      $this->log()->notice(
+        "Cloning database from {from_name} environment to {target_env} environment",
+        compact(['from_name', 'target_env'])
+      );
+      while (!$workflow->checkProgress()) {}
+      $this->log()->notice($workflow->getMessage());
+    }
+
+    $this->log()->notice('Running Drush Clear Cache All');
+    $this->sendDrushCommand($env, 'cc all');
+    $this->log()->notice('Running Drush Updatedb');
+    $this->sendDrushCommand($env, 'updatedb -y');
+    $this->log()->notice('Running Drush Clear Cache All');
+    $this->sendDrushCommand($env, 'cc all');
+
+    if(!is_null($options['username'])) {
+      $this->sendDrushCommand($env, "sql-query \"UPDATE users SET name='{$options['username']}' WHERE uid=1\"");
+      $this->log()->notice('{env} updated user 1 username to {username}', ['env' => $env, 'username' => $options['username']]);
+    }
+
+    if(!is_null($options['username']) && !is_null($options['password'])) {
+      $this->sendDrushCommand($env, "user-password \"{$options['username']}\" --password=\"{$options['password']}\"");
+      $this->log()->notice('{env} updated user 1 password to {password}', ['env' => $env, 'password' => $options['password']]);
+    }
+
+    if(!is_null($options['slack_url'])){
+      $client = new Client($options['slack_url'], [
+        'channel' => $options['slack_channel'],
+        'username' => $options['slack_username'],
+        'icon' => $options['slack_icon']
+      ]);
+
+      $url = $this->message("http://{env}-{site_id}.pantheonsite.io", ['env' => $env, 'site_id' => $site_id]);
+      $message = $options['slack_message'];
+      if(is_null($message)){
+        $message = "<{url}|{site_name} - {env}> Environment created with upstream tests";
+      }
+
+      $message = $this->message($message, ['env' => $env, 'site_id' => $site_id, 'site_name' => $site_name, 'url' => $url]);
+      $client->send($message);
+      $this->log()->notice('Message sent to slack channel: {url}: {channel}', ['url' => $options['slack_url'], 'channel' => $options['slack_channel']]);
+    }
+
+    $this->log()->notice('{site}: Upstream Test Push Completed', ['site' => $site_name]);
+  }
+
+  /**
+   * Short hand function to pass Drush Commands
+   */
+  protected function sendDrushCommand($env, $command){
+    return $this->site->getEnvironments()->get($env)->sendCommandViaSsh('drush '.$command);
+  }
+
+  /**
+   * Short hand function to test if environment is initialized.
+   * @param String $env
+   * @return boolean
+   */
+  protected function isInitialized($env){
+    return $this->site->getEnvironments()->get($env)->isInitialized();
+  }
+
+  /**
+   * Replace the variables into the message string.
+   *
+   * @param string $message      The raw, uninterpolated message string
+   * @param array  $replacements The values to replace into the message
+   * @return string
+   */
+  protected function message($message, $replacements)
+  {
+    $tr = [];
+    foreach ($replacements as $key => $val) {
+      $tr['{' . $key . '}'] = $val;
+    }
+    return strtr($message, $tr);
+  }
+
+  /**
+   * Call passthru; throw an exception on failure.
+   *
+   * @param string $command
+   */
+  protected function passthru($command, $loggedCommand = '')
+  {
+    $result = 0;
+    $loggedCommand = empty($loggedCommand) ? $command : $loggedCommand;
+    $this->log()->notice("Running {cmd}", ['cmd' => $loggedCommand]);
+    passthru($command, $result);
+    if ($result != 0) {
+      throw new TerminusException('Command `{command}` failed with exit code {status}', ['command' => $loggedCommand, 'status' => $result]);
+    }
+  }
+}
